2ee0ecb89760787d83909ca1829160041edf8b88
Davy Jones <hello@galleon.community>
2023-12-18 22:30:47 +0000

    Update Articles.tsx

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -49,7 +49,7 @@ const Articles = ({ config }: ArticlesProps) => {
           <li key={index} className="col-span-1 divide divide-theme-yellow rounded-sm bg-theme-gray   ">
             <a href={article.link} target={'blank'} className="flex w-full items-center justify-between space-x-6 p-4 rounded-sm bg-theme-yellow/10 hover:bg-theme-yellow/20">
               <div className="flex-1">
-                <h3 className="text-lg font-semibold truncate  text-theme-yellow pb-1 mb-2 border-b border-theme-yellow">{article.name}</h3>
+                <h3 className="text-lg font-semibold sm:whitespace-nowrap whitespace-normal   text-theme-yellow pb-1 mb-2 border-b border-theme-yellow">{article.name}</h3>
                 <p className="py-0.5 ml-0.5 inline-flex flex-shrink-0 items-center rounded-sm px-2  text-md text-theme-yellow">
                   {article.subtitle}
                 </p>
@@ -61,7 +61,7 @@ const Articles = ({ config }: ArticlesProps) => {
         ))}
 
       </ul>
-
+      <iframe className="mx-auto -mt-8 min-w-full"  src="https://tortugaonchain.substack.com/embed" ></iframe>
     </div>
   );
 };
